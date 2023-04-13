7f1b42f3ecff4eab1089b0c15289d5031bc23e05
Eshika rawat <eshikarawat02@gmail.com>
2023-04-13 11:37:25 +0530

    added all files

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Footer from './../Footer/Footer'
+import Header from './../Header/Header'
+
+import Routers from '../../router/Routers';
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+  return <>
+  <Header />
+  <Routers />
+  <Footer />
+   
+  </>
 }
 
 export default Layout
